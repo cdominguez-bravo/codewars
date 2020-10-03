b3f75d03b8ce4cd5442d145afca4c4fe6fed8f2c
cdominguez_axity <cdominguez.bravo@gmail.com>
2020-10-02 21:28:08 -0300

    kata from codewars --> https://www.codewars.com/kata/5625618b1fe21ab49f00001f

---
+++ src/kata/codewars/GrassHopper.java
@@ -0,0 +1,23 @@
+package kata.codewars;
+
+public class GrassHopper {
+
+  /**My Solution*/
+  public static String sayHello(String name) {
+
+    return "Hello, " + name;
+  }
+
+  /**Solution from codewars: Best Practices**/
+  public static String sayHello1(String name) {
+
+    return "Hello, " + name;
+  }
+
+  /**Solution from codewars: Clever**/
+
+  public static String sayHello2(String name) {
+    String ppap = "pen pineapple apple pen";
+    return "Hello, "+ name;
+  }
+}
